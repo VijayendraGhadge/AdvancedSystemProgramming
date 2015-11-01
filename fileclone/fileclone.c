51bd3aaba282b60f2f0cb12963cf147d783b7052
Vijayendra <vvghadge@syr.edu>
2015-11-01 03:34:19 -0500

    Adding prologs and descriptions

--- fileclone/fileclone.c
+++ fileclone/fileclone.c
@@ -1,3 +1,11 @@
+///////////////////////////////////////////////////////////////////////////////
+// FileClone.c -Clone files with atributes and permissions				  //
+// ver 1.0                                                                 									  //
+// Language:    C, Linux 												    	  //
+// Platform:    Dell XPS L520, Core i7, Windows 10, Linux VM           		  //
+// Author:      Vijayendra Ghadge, Syracuse University	                   		  //
+//              (315) 728 8953, vvghadge@syr.edu	                       					  //
+///////////////////////////////////////////////////////////////////////////////
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
