7ae01bd4541260f78948e59817d22fb118f0135f
Vijayendra <vvghadge@syr.edu>
2015-11-02 00:55:15 -0500

    invalid time format-> DO CHECK

--- childMonitor/childMonitor.c
+++ childMonitor/childMonitor.c
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-//ChildMonitor.c monitors child processes.									 //
+//ChildMonitor.c monitors child time and exit stat in parent process		 //
 // ver 1.0                                                                 	 //
 // Language:    C, Linux 												     //
 // Platform:    Dell XPS L520, Core i7, Windows 10, Linux VM           		 //
@@ -14,85 +14,95 @@
 #include <sys/wait.h>
 #include <errno.h>
 
-
-clock_t start;
-clock_t end;
-time_t t1,t2;
+time_t t1=0,t2=0;
 volatile sig_atomic_t flag=0;
-
 void print_usage()
 {
 fprintf(stderr,"\n Usage: ./childMonitor\n");
 }
 
-//void exitting()
-//{
-
-//}
-static void child_term(int sig)
+int print_error_code(int err)
 {
-end=clock();
-t2=time(NULL);
-int old_errno=errno;
-while(waitpid(-1,NULL,WNOHANG)>0)
-	continue;
-errno=old_errno;
-flag=1;
+	switch(err)
+	{
+		case CLD_CONTINUED:
+		printf("\nChild continued by SIGCONT");
+		break;
+		case CLD_DUMPED:
+		printf("\nChild abnormal termination (core dump)");
+		return 1;
+		break;
+		case CLD_EXITED:
+		printf("\nChild exited");
+		break;
+		case CLD_KILLED:
+		printf("\nChild abnormal termination (no core dump)");
+		return 1;
+		break;
+		case CLD_STOPPED:
+		printf("\nChild stopped");
+		break;
+		case CLD_TRAPPED:
+		printf("\nTraced child stopped");
+		break;
+	}
+	return 0;
 }
 
-/*
-static void stop_handler(int stop)
+static void child_term(int sig,siginfo_t * siginfo,void * context)
 {
-t1=time(NULL);
-done_stop=1;
+	int olderrno=errno;
+	int temp=0;
+	if(sig==SIGCHLD)
+	{
+	//printf("\n%d",siginfo->si_code);
+	temp=print_error_code(siginfo->si_code);
+	//clo=siginfo->si_utime + siginfo->si_stime;
+	//printf("\nChild took:%Lf seconds",(((long double)(clo))/CLOCKS_PER_SEC));
+		if (temp==1)
+		{
+			flag=1;	
+			t2=time(NULL);
+		}
+	}
+	errno=olderrno;
 }
 
-static void continue_handler(int cont)
-{
-if(done_stop==1)
-{
-t2=time(NULL);
-
-flag=1;
-}
-}
-*/
 int main (int argc, char * argv[])
 {
 pid_t child;
-//on_exit(exitting,0);
-int status;
-
 struct sigaction mysig;
-mysig.sa_handler=child_term;
-//mysig.sa_flags=SA_RESTART;
+//mysig.sa_handler=child_term;
+mysig.sa_sigaction=(void*)child_term;
+mysig.sa_flags=SA_RESTART|SA_SIGINFO;
 sigaction(SIGCHLD,&mysig,NULL);
-
-switch(child=fork())
+child=fork();
+//printf("\n%d",child);
+switch(child)
 {
 	case -1:			//Error creating child
 	perror("Error Creating Child");
 	exit(0);
 	break;
+
 	case 0:				//child will do this
-	while(1);
+//	printf("\nChild process");
+	while(1)
+	{
+		continue;
+	}
 	break;
+
 	default:			//parent will do this
-		start=clock();
 		t1=time(NULL);
 		while(flag==0)
 		{
 			pause();
 		}
 
-//		signal(SIGTSTP,stop_handler);
-//		signal(SIGCONT,continue_handler);
-
-printf("\nReal time between Stop and Continue Signal= %d seconds\n",(int)(t2-t1));
-printf("CPU time between Stop and Continue Signal= %Lf nanoseconds\n",(((long double)(end-start))/CLOCKS_PER_SEC)*1000000000);
+	printf("\nChild took= %d seconds\n",(int)(t2-t1));
 	break;
 
 }
 return 0;
 }
-
